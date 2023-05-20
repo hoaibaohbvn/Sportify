b3c6021513022a89c9364209dd83fdb01ee8ddd3
hoaibaohbvn <domaihoaibao@gmail.com>
2023-05-20 19:25:45 +0700

    Initial commit

---
+++ src/main/java/duan/sportify/SportifyApplication.java
@@ -0,0 +1,13 @@
+package duan.sportify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duan/sportify/SportifyApplicationTests.java
@@ -0,0 +1,13 @@
+package duan.sportify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
