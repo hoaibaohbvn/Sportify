58fde3216c691717cead816349c3562317f2328b
hoaibaohbvn <domaihoaibao@gmail.com>
2023-06-14 09:26:56 +0700

    fill database to web

--- src/main/java/duan/sportify/dao/EventDAO.java
+++ src/main/java/duan/sportify/dao/EventDAO.java
@@ -2,8 +2,9 @@ package duan.sportify.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import duan.sportify.entities.Events;
 
-public interface EventDAO extends JpaRepository<Events, Integer>{
+import duan.sportify.entities.Eventweb;
+
+public interface EventDAO extends JpaRepository<Eventweb, Integer>{
 
 }
--- src/main/java/duan/sportify/dao/FieldDAO.java
+++ src/main/java/duan/sportify/dao/FieldDAO.java
@@ -1,9 +1,15 @@
 package duan.sportify.dao;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import duan.sportify.entities.Fields;
 
-public interface FieldDAO extends JpaRepository<Fields, Integer>{
+import duan.sportify.entities.Field;
 
+
+
+public interface FieldDAO extends JpaRepository<Field, Integer>{
+//	@Query("SELECT bd.fieldID, COUNT(*) AS luot_dat FROM BookingDetails bd GROUP BY bd.fieldID ORDER BY luot_dat DESC")
+//    List<Object[]> getTop4BookingCounts();
 }
