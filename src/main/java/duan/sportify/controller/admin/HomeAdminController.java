1d0ea783a9772335008b563552f2c6d007686528
hoaibaohbvn <domaihoaibao@gmail.com>
2023-06-19 12:46:18 +0700

    Done Home

--- src/main/java/duan/sportify/controller/admin/HomeAdminController.java
+++ src/main/java/duan/sportify/controller/admin/HomeAdminController.java
@@ -1,14 +1,15 @@
-package duan.sportify.controller;
+package duan.sportify.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("sportify")
-public class EventController {
-	@GetMapping("event")
-	public String viewEvent() {
-		return "user/blog";
+@RequestMapping("/admin/sportify")
+public class HomeAdminController {
+	@GetMapping()
+	public String view() {
+		
+		return "admin/index";
 	}
 }
