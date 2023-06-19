0f6fe0b73eadbd70260ed2e19bf99232a3c680dd
PhamTu2001 <109650361+PhamTu2001@users.noreply.github.com>
2023-06-19 12:04:40 +0700

    Done view Field cơ bản
    - View các trường dữ liệu
    - Format double sang vnđ.

--- src/main/java/duan/sportify/controller/FieldController.java
+++ src/main/java/duan/sportify/controller/FieldController.java
@@ -1,6 +1,8 @@
 package duan.sportify.controller;
 
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
 
@@ -14,6 +16,7 @@ import duan.sportify.dao.FieldDAO;
 
 import duan.sportify.entities.Field;
 
+
 @Controller
 @RequestMapping("")
 public class FieldController {
@@ -22,8 +25,28 @@ public class FieldController {
 
 	@GetMapping("field")
 	public String viewField(Model model) {
+//		double vnd = 0;
+
+
 		List<Field> eventList = fieldDAO.findAll();
-		
+//		double[] vndArray = new double[eventList.size()];
+//		for(int i = 0; i < eventList.size(); i++) {
+//			 vnd = eventList.get(i).getPrice();
+//			 vndArray[i] = vnd;
+//				Locale locale = new Locale("vi", "VN");
+//		        Currency currency = Currency.getInstance("VND");
+//
+//		        DecimalFormatSymbols df = DecimalFormatSymbols.getInstance(locale);
+//		        df.setCurrency(currency);
+//		        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+//		        numberFormat.setCurrency(currency);
+//		        System.out.println(numberFormat.format(vnd));
+//		        model.addAttribute("vndArray",vndArray);
+//		}
+
+
+//		List<Field> eventList = fieldDAO.findAll();
+
 		model.addAttribute("fieldList", eventList);
 		return "user/san";
 	}
