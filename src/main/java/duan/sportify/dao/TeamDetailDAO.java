03e6f286e62abef6667bf983c4aa8319d752260e
MinhPham19 <congminh.190702@gmail.com>
2023-06-21 13:07:20 +0700

    update view đội V1.1

--- src/main/java/duan/sportify/dao/TeamDetailDAO.java
+++ src/main/java/duan/sportify/dao/TeamDetailDAO.java
@@ -1,6 +1,8 @@
 package duan.sportify.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import duan.sportify.entities.Teamdetails;
 
