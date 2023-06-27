c9bec0835a8f4f2e39765a4e0d0e1dca1871d4e9
Jerry Ghost <aloha140102@yahoo.com>
2023-06-27 15:33:10 +0700

    Revert "Revert "Merge branch 'Products' into branchOfThai""
    This reverts commit 1b328f7c026971103921d15c287ae247b22a3cbf.

--- src/main/java/duan/sportify/controller/ProductController.java
+++ src/main/java/duan/sportify/controller/ProductController.java
@@ -1,26 +1,36 @@
 package duan.sportify.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import duan.sportify.dao.ProductDAO;
 import duan.sportify.entities.Products;
+import duan.sportify.service.ProductService;
+
 @Controller
-@RequestMapping("/sportify")
+@RequestMapping("sportify")
 public class ProductController {
-	@Autowired 
+	
+	
+	@Autowired
 	ProductDAO productDAO;
 	
 	@GetMapping("product")
-	public String list(Model model) {
-	List<Products> list = productDAO.findAll();
-	model.addAttribute("productList",list);
+	//view all, đổ dữ liệu
+	public String viewProduct(Model model) {
+		List<Products> productList = productDAO.findAll();
+		model.addAttribute("productList", productList);
 		return "user/product";
 	}
 }
