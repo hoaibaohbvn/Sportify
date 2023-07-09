8f5665512787231d6ea45d3c74f08df241a42ac5
MinhPham19 <congminh.190702@gmail.com>
2023-07-09 10:13:07 +0700

    update limited-length
    giới hạn kí tự trong ghi chú

--- src/main/java/duan/sportify/controller/TeamController.java
+++ src/main/java/duan/sportify/controller/TeamController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import duan.sportify.dao.TeamDAO;
+import duan.sportify.entities.Teams;
+import duan.sportify.service.TeamDetailService;
 
 @Controller
 @RequestMapping("/sportify")
