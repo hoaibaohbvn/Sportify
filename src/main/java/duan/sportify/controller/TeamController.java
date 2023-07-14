59fdd93a3078f91148a4cb6a0f5d7283ff01af57
MinhPham19 <congminh.190702@gmail.com>
2023-07-14 08:06:24 +0700

    update team V1.4

--- src/main/java/duan/sportify/controller/TeamController.java
+++ src/main/java/duan/sportify/controller/TeamController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import duan.sportify.dao.TeamDAO;
+import duan.sportify.dao.TeamDetailDAO;
 import duan.sportify.entities.Sporttype;
 import duan.sportify.service.SportTypeService;
 
@@ -23,23 +26,23 @@ public class TeamController {
 	
 	@Autowired
 	SportTypeService sportTypeService;
+	
+	//Đỗ lọc
+	@ModelAttribute("sporttypeList")
+	public List<Sporttype> getSporttypeList() {
+	    return sportTypeService.findAll();
+	}
 
 	// Đỗ toàn bộ dữ liệu liên quan đến team
 	@GetMapping("/team")
 	public String viewTeam(Model model) {
 		List<Object[]> listall = teamdao.findAllTeam();
 		model.addAttribute("team", listall);
-		List<Sporttype> sporttypeList = sportTypeService.findAll();
-		model.addAttribute("sporttype",sporttypeList);
-		System.out.println(sporttypeList.size());
 		return "user/doi";
 	}
-	// Đỗ dự liệu đã tìm kiếm 
+	// tìm kiếm 
 	@PostMapping("team/search")
 	public String search(Model model, @RequestParam("SearchText") String searchText) {
-		//kiểm tra xem đã lấy dc searchText
-		//System.out.println(searchText);
-	    model.addAttribute("searchText", searchText);
 		//Tìm kiếm theo searchText
 	    List<Object[]> teams = teamdao.SearchTeam(searchText);
 	    model.addAttribute("team", teams);
@@ -54,4 +57,27 @@ public class TeamController {
 	    	model.addAttribute("FoundMessage","Tìm thấy "+teams.size()+" kết quả tìm kiếm của '"+searchText+"'");
 	    }
 		return "user/doi";	}
+	
+	
+	 @GetMapping("/team/{sporttypeid}")
+	    public String handleSportifyTeam(Model model, @PathVariable("sporttypeid") String sporttypeid) {
+		 	String cid=sporttypeid;
+	        List<Object[]> filterName = teamdao.FilterTeam(sporttypeid);
+		    model.addAttribute("team", filterName);		    
+	        return "user/doi";
+	    }
+	 
+	 
+	 @Autowired
+	 TeamDetailDAO detailDAO;
+	 
+	 @GetMapping("team/teamdetail/{teamId}")
+		public String teamdetail(Model model, @PathVariable("teamId") String teamId) {
+		 	String Tid=teamId;
+	        System.out.println(Tid);
+	        List<Object[]> listall = detailDAO.findByIdTeam(Tid);
+			model.addAttribute("team", listall);
+			return "user/team-single";	}
 }
+
+	
