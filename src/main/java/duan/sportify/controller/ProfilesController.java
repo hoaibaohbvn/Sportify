c748ce88d445d973dc33c798bacb49c61e93f330
MinhPham19 <congminh.190702@gmail.com>
2023-07-23 14:30:49 +0700

    update V1.0
    - đỗ dự liệu người dùng đang đăng nhập lên form
    - chỉnh sửa giao diện

--- src/main/java/duan/sportify/controller/ProfilesController.java
+++ src/main/java/duan/sportify/controller/ProfilesController.java
@@ -1,15 +1,28 @@
 package duan.sportify.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import duan.sportify.entities.Users;
+import duan.sportify.service.UserService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("sportify")
 public class ProfilesController {
+	@Autowired
+	UserService userService;
+	
 	@GetMapping("/profile")
-	public String view() {
-		
+	public String view(Model model, HttpSession session) {
+		//Kiểm tra đăng nhập
+		Users loggedInUser = (Users) session.getAttribute("loggedInUser");
+		String userName=loggedInUser.getUsername();
+		Users profiles = userService.findById(userName);
+		model.addAttribute("prodiles",profiles);
 		return "/user/profiles";
 	}
 }
