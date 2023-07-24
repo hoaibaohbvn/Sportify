07b3d342b7a7bb50626e9ed8dbc61a12bd31196b
MinhPham19 <congminh.190702@gmail.com>
2023-07-24 13:56:42 +0700

    Done Profiles
    -Thay đổi anh đại diện
    -Chỉnh giao diện

--- src/main/java/duan/sportify/controller/ProfilesController.java
+++ src/main/java/duan/sportify/controller/ProfilesController.java
@@ -1,5 +1,10 @@
 package duan.sportify.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import duan.sportify.dao.UserDAO;
@@ -38,27 +44,36 @@ public class ProfilesController {
 
 	@PostMapping("/profile/save-profile")
 	public String saveProfiles(Model model, HttpSession session, RedirectAttributes redirectAttributes,
-			@RequestParam String firstname, @RequestParam String lastname, @RequestParam String passwords,
-			@RequestParam String phone, @RequestParam String email, @RequestParam Boolean gender,
-			@RequestParam String address) {
+			@RequestParam("avatarFile") MultipartFile avatarFile, @RequestParam String firstname,
+			@RequestParam String lastname, @RequestParam String passwords, @RequestParam String phone,
+			@RequestParam String email, @RequestParam Boolean gender, @RequestParam String address) throws IOException {
 
 		Users loggedInUser = (Users) session.getAttribute("loggedInUser");
-		if(loggedInUser!=null) {
-			String userName = loggedInUser.getUsername();
-				Users updateUser = userService.findById(userName);
-				updateUser.setFirstname(firstname);
-				updateUser.setLastname(lastname);
-				updateUser.setPasswords(passwords);
-				updateUser.setPhone(phone);
-				updateUser.setEmail(email);
-				updateUser.setGender(gender);
-				updateUser.setAddress(address);
-				userDAO.save(updateUser);
-				redirectAttributes.addFlashAttribute("message", "Cập nhật thông tin thành công !");
-				return "redirect:/sportify/profile";
-			} else {
-				redirectAttributes.addFlashAttribute("message1", "Cập nhật thông tin thất bại!");
-				return "redirect:/sportify/profile";
+		String userName = loggedInUser.getUsername();
+		Users updateUser = userService.findById(userName);
+		// Xử lý tải lên ảnh
+		if (!avatarFile.isEmpty()) {
+			try {
+				// Thay đổi đường dẫn tới thư mục lưu trữ ảnh (thay thế "/" bằng đường dẫn thư
+				// mục thực tế)
+				String uploadDir = "C:\\Users\\MSI GAMING\\Documents\\GitHub\\Sportify\\src\\main\\resources\\static\\user\\images";
+				String fileName = avatarFile.getOriginalFilename();
+				Path filePath = Paths.get(uploadDir, fileName);
+				Files.write(filePath, avatarFile.getBytes());
+				updateUser.setImage(fileName);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+		}
+		updateUser.setFirstname(firstname);
+		updateUser.setLastname(lastname);
+		updateUser.setPasswords(passwords);
+		updateUser.setPhone(phone);
+		updateUser.setEmail(email);
+		updateUser.setGender(gender);
+		updateUser.setAddress(address);
+		userDAO.save(updateUser);
+		redirectAttributes.addFlashAttribute("message", "Cập nhật thông tin thành công!");
+		return "redirect:/sportify/profile";
 	}
 }
