51e1f3666ef8a130b3ee16cbc5dde6badd4c2038
NeoD99 <117890355+NeoD99@users.noreply.github.com>
2023-07-26 14:30:28 +0700

    test3

--- src/main/java/duan/sportify/entities/Eventweb.java
+++ src/main/java/duan/sportify/entities/Eventweb.java
@@ -50,6 +50,7 @@ public class Eventweb implements Serializable {
 
     @Column(name="descriptions", length=1000)
     private String     descriptions ;
+    
     @Column(name="eventtype", length=50)
     private String     eventtype ;
 
