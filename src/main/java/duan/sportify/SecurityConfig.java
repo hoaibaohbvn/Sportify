0ba22bd5e6d41cd56f565e408e22577205b61449
hoaibaohbvn <domaihoaibao@gmail.com>
2023-07-29 17:11:32 +0700

    Upload Done

--- src/main/java/duan/sportify/SecurityConfig.java
+++ src/main/java/duan/sportify/SecurityConfig.java
@@ -24,70 +24,83 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import duan.sportify.dao.UserDAO;
 import duan.sportify.entities.Users;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	BCryptPasswordEncoder pe;
-
 	@Autowired
 	UserDAO userDAO;
-
+	@Autowired
+	HttpSession session;
+	
 	// Cung cấp nguồn dữ liệu đăng nhập
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(username -> {
-			try {
-				Users user = userDAO.findById(username).get();
-
-				String password = pe.encode(user.getPasswords());
-
-				String[] roles = user.getListOfAuthorized().stream()
-						.map(er -> er.getRoles().getRoleid())
-						.collect(Collectors.toList())
-						.toArray(new String[0]);
-				
-				System.out.println(password);
-				return User.withUsername(username).password(password).roles(roles).build();
-
-			} catch (NoSuchElementException e) {
-				throw new UsernameNotFoundException(username + "Not Found");
-			}
-		});
-	}
-
-	// Phân quyền
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// CSRF,CORS
-		http.csrf().disable().cors().disable();
-
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.userDetailsService(username -> {
+				try {
+					Users user = userDAO.findById(username).get();
+					String password = pe.encode(user.getPasswords());
+					System.out.println(user.getUsername());
+					System.out.println(password);
+					String[] roles = user.getListOfAuthorized().stream()
+							.map(er -> er.getRoles().getRoleid())
+							.collect(Collectors.toList())
+							.toArray(new String[0]);
+					
+					Map<String, Object> authentication = new HashMap<>();
+					authentication.put("user", user);
+					byte[] token = (username + ":" + user.getPasswords()).getBytes();
+					authentication.put("token", "Basic " + Base64.getEncoder().encodeToString(token));
+					session.setAttribute("authentication", authentication);
+					
+					return User.withUsername(username).password(password).roles(roles).build();
+				} catch (NoSuchElementException e) {
+					throw new UsernameNotFoundException(username + " not found!");
+				}
+			});
+			
+		}
+		
 		// Phân quyền sử dụng
-		http.authorizeRequests()
-		.antMatchers("/rest/authorities").hasRole("admin")
-		.antMatchers("/admin/**").hasAnyRole("admin", "employee")
-		.antMatchers("/discount/**").authenticated().anyRequest().permitAll();
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {	
+			http.csrf().disable().cors().disable();
+			http.authorizeRequests()
+				.antMatchers("/sportify/field/booking/**").authenticated()
+				.antMatchers("/admin/**").hasAnyRole("R01", "R02")
+				.antMatchers("/rest/authorities").hasRole("R01")
+				.anyRequest().permitAll();
+			
+			http.formLogin()
+				.loginPage("/sportify/login/form")		
+				.loginProcessingUrl("/sportify/login")
+				.defaultSuccessUrl("/sportify/login/success", false)
+				.failureUrl("/sportify/login/error")
+				.usernameParameter("username").passwordParameter("password");
+				
 
-		// Giao diện login
-		http.formLogin(login -> login.loginPage("/account/login/form").loginProcessingUrl("/account/login")
-				.defaultSuccessUrl("/account/login/success", false).failureUrl("/account/login/error")
-				.usernameParameter("email-username").passwordParameter("password"));
-		http.rememberMe(me -> me.rememberMeParameter("remember"));
+			http.rememberMe()
+				.tokenValiditySeconds(86400);
+			
+			http.exceptionHandling()
+				.accessDeniedPage("/sportify/unauthoried");
 
-		http.exceptionHandling(handling -> handling.accessDeniedPage("/account/unauthoried"));
-		// Đăng xuất
-		http.logout(logout -> logout.logoutUrl("/account/logoff").logoutSuccessUrl("/account/logoff/success"));
-	}
-
-	// Mã hóa mk
-	@Bean
-	public BCryptPasswordEncoder getBCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	// Cho phép truy xuất REST API từ bên ngoài
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-	}
-
+			http.logout()
+				.logoutUrl("/sportify/logoff")
+				.logoutSuccessUrl("/sportify/logoff/success");		
+		}
+		
+		// Cơ chế mã hóa mật khẩu
+		@Bean
+		public BCryptPasswordEncoder getPasswordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+		
+		// Cho phép truy xuất REST API từ domain khác
+		@Override
+	    public void configure(WebSecurity web) throws Exception {
+	        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+	    }
 }
