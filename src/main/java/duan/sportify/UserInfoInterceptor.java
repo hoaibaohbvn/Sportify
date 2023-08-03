93fc893bd63688d041a3b5a74b401b8a01cda470
MinhPham19 <congminh.190702@gmail.com>
2023-08-03 16:20:13 +0700

    update check role

--- src/main/java/duan/sportify/UserInfoInterceptor.java
+++ src/main/java/duan/sportify/UserInfoInterceptor.java
@@ -13,25 +13,32 @@ import javax.servlet.http.HttpSession;
 
 import duan.sportify.service.UserService;
 import duan.sportify.entities.Users;
+import duan.sportify.entities.Authorized;
+import duan.sportify.dao.AuthorizedDAO;
 
 @Component
 public class UserInfoInterceptor implements HandlerInterceptor {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private AuthorizedDAO authorizedDAO; 
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // Lấy username từ session
         String username = (String) request.getSession().getAttribute("username");
-
         if (username != null) {
             // Lấy thông tin người dùng từ service
             Users users = userService.findById(username);
             // Đưa thông tin người dùng vào model
             request.setAttribute("users", users);
+            //lấy role người dùng
+            Authorized authorized= authorizedDAO.findAllAuthorized(username);
+            // Đưa thông tin vào model
+            request.setAttribute("authorized", authorized);
         }
-
         return true;
     }
 }
