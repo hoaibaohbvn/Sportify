16d9d3f81bed94c0c03d79bb3d3ec35ccdf49227
PhamTu2001 <109650361+PhamTu2001@users.noreply.github.com>
2023-08-09 15:47:12 +0700

    Update Security Order

--- src/main/java/duan/sportify/SecurityConfig.java
+++ src/main/java/duan/sportify/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		protected void configure(HttpSecurity http) throws Exception {	
 			http.csrf().disable().cors().disable();
 			http.authorizeRequests()
-				.antMatchers("/sportify/field/booking/**","/sportify/field/profile/**","/sportify/team/teamdetail/**","/sportify/submit-contact" ).authenticated()
+				.antMatchers("/sportify/field/booking/**","/sportify/field/profile/**","/sportify/team/teamdetail/**","/sportify/submit-contact","/sportify/order/checkout").authenticated()
 				.antMatchers("/admin/**").hasAnyRole("R01", "R02")
 				.antMatchers("/rest/authorities").hasRole("R01")
 				.anyRequest().permitAll();
