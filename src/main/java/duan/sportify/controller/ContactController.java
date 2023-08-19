04b457a5d530125cd46c0eb28e7ff0fe1af2d856
NeoD99 <117890355+NeoD99@users.noreply.github.com>
2023-08-19 14:48:07 +0700

    gioihansolanguicontact

--- src/main/java/duan/sportify/controller/ContactController.java
+++ src/main/java/duan/sportify/controller/ContactController.java
@@ -2,7 +2,9 @@ package duan.sportify.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
@@ -35,32 +37,48 @@ public class ContactController {
 	@Autowired
 	UserService userService;
 	String userlogin;
+
 	@GetMapping("contact")
-	public String view(Model model, HttpServletRequest request ) {
-		 userlogin = (String) request.getSession().getAttribute("username");
-		 model.addAttribute("contacts", new Contacts());
-//		model.addAttribute("user", new Contacts());
-	return "user/contact"; 
+	public String view(Model model, HttpServletRequest request) {
+		userlogin = (String) request.getSession().getAttribute("username");
+		model.addAttribute("contacts", new Contacts());
+		return "user/contact";
 	}
-	
-    @PostMapping("/submit-contact")
-    public String processContactForm(Model model, HttpSession session, RedirectAttributes redirectAttributes, @RequestParam("contactType") String contactType,
-			@RequestParam String title, @RequestParam String meesagecontact, @Valid Contacts contacts, BindingResult result) {
-    	if (result.hasErrors()) {
-            return "user/contact"; // Return the form view to show validation errors
-        }
-    	
-    	contacts.setUsername(userlogin);
-    	contacts.setTitle(title);
-    	contacts.setMeesagecontact(meesagecontact);
-    	contacts.setCategory(contactType);
-    	LocalDate localDate = LocalDate.now();
-    	Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-    	contacts.setDatecontact(date);
-    	contactDAO.save(contacts);
-    	redirectAttributes.addFlashAttribute("message", "Soprtify Cảm ơn bạn đã phản hồi");
-    	return "redirect:/sportify/contact"; // Hoặc điều hướng đến trang khác sau khi đã lưu thành công.
-    }
-	
-	
+
+	@PostMapping("/submit-contact")
+	public String processContactForm(Model model, HttpSession session, RedirectAttributes redirectAttributes,
+			@RequestParam("contactType") String contactType, @RequestParam String title,
+			@RequestParam String meesagecontact, @Valid Contacts contacts, BindingResult result) {
+		if (result.hasErrors()) {
+			return "user/contact"; // Return the form view to show validation errors
+		}
+//		LocalDateTime lastSubmissionTime = getLastSubmissionTimeForUser(userlogin); // Get the last submission time for the user from your storage
+//		LocalDateTime currentTime = LocalDateTime.now();
+//		Duration timeSinceLastSubmission = Duration.between(lastSubmissionTime, currentTime);
+//
+//		if (timeSinceLastSubmission.toHours() < 24) {
+//			// User cannot submit a new response within 24 hours
+//			redirectAttributes.addFlashAttribute("message", "Bạn chỉ có thể gửi phản hồi mới sau 24 giờ.");
+//			return "redirect:/sportify/contact";
+//		}
+		List<String> listcontacted = contactDAO.contactedInDay();
+		String loggedInUsername = userlogin;
+		if(listcontacted.contains(loggedInUsername)) {
+			 redirectAttributes.addFlashAttribute("message", "Bạn chỉ có thể gửi phản hồi mới sau 24 giờ.");
+		        return "redirect:/sportify/contact";
+		}else {
+		// Lưu thông tin
+			contacts.setUsername(userlogin);
+			contacts.setTitle(title);
+			contacts.setMeesagecontact(meesagecontact);
+			contacts.setCategory(contactType);
+			LocalDate localDate = LocalDate.now();
+			Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+			contacts.setDatecontact(date);
+			contactDAO.save(contacts);
+			redirectAttributes.addFlashAttribute("message", "Sportify Cảm ơn bạn đã phản hồi");
+			return "redirect:/sportify/contact"; // Chuyển đến trang contact khi đã lưu thành công.
+		}
+	}
+
 }
